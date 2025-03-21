d529b2e68041c184220ae256cd7f0f0231692c14
LuckyHubsi <cc221040@fhstp.ac.at>
2025-03-21 12:13:12 +0100

    Add initial setup

---
+++ backend/src/main/java/com/medialab/krizzle/KrizzleApplication.java
@@ -0,0 +1,13 @@
+package com.medialab.krizzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrizzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrizzleApplication.class, args);
+	}
+
+}
