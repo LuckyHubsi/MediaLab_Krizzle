3810a4d51b88e43b8a15ccbbd3653f4d06614463
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-03-28 10:17:43 +0100

    refactor(delete): delete unnecessary files
    Delete all unnecessary files from expo demo

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,19 +1,19 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { useColorScheme } from '@/hooks/useColorScheme'; // Add this import
+import { Colors } from '@/constants/Colors'; // Add this import
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const colorScheme = useColorScheme(); // light or dark
+  const color = Colors[colorScheme ?? 'light'].tint;
+
   return (
-       <SafeAreaView>
-               <ThemedText type="title">Dashboard</ThemedText>
-               <ThemedText>Lorem ipsum</ThemedText>
-       </SafeAreaView>
+      <SafeAreaView>
+        <ThemedText type="title">Home</ThemedText>
+        <IconSymbol name="person" size={32} color={color} />
+      </SafeAreaView>
   );
 }
-
-
