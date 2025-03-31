1ac443fecfff9f25e6b81ed0d35349c756c1e371
seoyangjadu <plum.blossom.haejin@gmail.com>
2025-03-31 15:36:26 +0200

    feat(GeneralPageModel): change page model
    Change date related data types to Date

--- models/GeneralPageModel.ts
+++ models/GeneralPageModel.ts
@@ -1,17 +1,19 @@
+import { Tag } from './TagModel';
+
 export interface GeneralPage {
     pageID: number;
     //pageType: PageType;
     pageTitle: string;
     //pageIcon: PageIcon;
     //pageColor: PageColor;
-    dateCreated: string;
-    dateModified: string;
+    dateCreated: Date;
+    dateModified: Date;
     archived: boolean;
     pinned: boolean;
-    pageTags: {tagID: number}[] | null;
+    pageTags: Tag[] | null;
 }
 
 export class GeneralPage {
-    constructor(public pageID: number, public pageTitle: string, public dateCreated: string, public dateModified: string,
-    public archived: boolean, public pinned: boolean, public pageTags: {tagID: number}[] | null) {}
+    constructor(public pageID: number, public pageTitle: string, public dateCreated: Date, public dateModified: Date,
+    public archived: boolean, public pinned: boolean, public pageTags: Tag[] | null) {}
 }
