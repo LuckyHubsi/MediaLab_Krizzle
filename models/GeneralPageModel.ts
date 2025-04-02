9641a78f68fd4a84bddd781afc7418fdb38e23f8
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-02 21:13:22 +0200

    refactor(entity models): refactor GeneralPageModel
    Refactoor GeneralPageModel due to changed conventions.
    
    Refs: SCRUM-5

--- models/GeneralPageModel.ts
+++ models/GeneralPageModel.ts
@@ -1,7 +1,8 @@
-import { Tag } from "./TagModel";
-
-export interface GeneralPage {
-    pageID: number;
+/**
+ * Represents a General Page Entity Model used for database interactions.
+ */
+export class GeneralPageModel {
+    readonly pageID?: number;
     page_type: string;
     page_title: string;
     page_icon: string;
@@ -10,10 +11,26 @@ export interface GeneralPage {
     date_modified: string;
     archived: number;
     pinned: number;
-    tag: Tag | null;
-}
 
-export class GeneralPage {
-    constructor(public pageID: number, public page_type: string, public page_title: string, public page_icon: string, public page_color: string,
-        public date_created: string, public date_modified: string, public archived: number, public pinned: number, public tag: Tag | null) {}
+    constructor(
+        page_type: string,
+        page_title: string,
+        page_icon: string,
+        page_color: string,
+        date_created: string,
+        date_modified: string,
+        archived: number,
+        pinned: number,
+        pageID?: number
+    ) {
+        this.pageID = pageID;
+        this.page_type = page_type;
+        this.page_title = page_title;
+        this.page_icon = page_icon;
+        this.page_color = page_color;
+        this.date_created = date_created;
+        this.date_modified = date_modified;
+        this.archived = archived;
+        this.pinned = pinned;
+    }
 }
