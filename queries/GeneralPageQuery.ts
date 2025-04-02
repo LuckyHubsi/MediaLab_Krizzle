71ef7f63140578ed1f32a242b2bb1bf08110bcb8
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-02 21:20:09 +0200

    feat(queries): add insert GeneralPage query
    Add query string for inserting new general_page_data into the DB.
    
    Refs: SCRU-5

--- queries/GeneralPageQuery.ts
+++ queries/GeneralPageQuery.ts
@@ -2,6 +2,11 @@ const generalPageSelectAllQuery: string = `
     SELECT * FROM general_page_data
 `;
 
+const insertNewPage: string = `
+    INSERT INTO general_page_data (page_type, page_title, page_icon, page_color, date_created, date_modified, archived, pinned) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+`;
+
 export {
-    generalPageSelectAllQuery
+    generalPageSelectAllQuery,
+    insertNewPage
 }
