5ee6027c33089406d0747d3bffc3f903b3d791b2
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-02 21:40:41 +0200

    feat(documentation): add doc to query helpers
    Add documentation to query helper functions.
    
    Refs: SCRUM-5

--- utils/QueryHelper.ts
+++ utils/QueryHelper.ts
@@ -2,6 +2,11 @@ import * as SQLite from 'expo-sqlite';
 
 let db: SQLite.SQLiteDatabase | null = null;
 
+/**
+ * Initializes and retrieves the SQLite database connection.
+ *
+ * @returns {Promise<SQLite.SQLiteDatabase>} A promise that resolves to the SQLite database instance.
+ */
 const getDb = async () => {
   if (!db) {
     db = await SQLite.openDatabaseAsync('krizzle_local.db');
@@ -9,11 +14,27 @@ const getDb = async () => {
   return db;
 };
 
+/**
+ * Executes an SQL query with optional parameters.
+ *
+ * @param {string} query - The SQL query to execute.
+ * @param {any[]} [params=[]] - The parameters for the query (optional).
+ * @returns {Promise<SQLite.RunResult>} A promise that resolves when the query is executed.
+ */
 const executeQuery = async (query: string, params: any[] = []) => {
     const db = await getDb();
     return db.runAsync(query, params);
 };
   
+
+/**
+ * Fetches the first result from an SQL query.
+ *
+ * @template T - The expected return type of the fetched data.
+ * @param {string} query - The SQL query to execute.
+ * @param {any[]} [params=[]] - The parameters for the query (optional).
+ * @returns {Promise<T | null>} A promise that resolves to the first row of the query result, or null if no data is found.
+ */
 const fetchFirst = async <T>(query: string, params: any[] = []): Promise<T | null> => {
     const db = await getDb();
     try {
@@ -25,6 +46,14 @@ const fetchFirst = async <T>(query: string, params: any[] = []): Promise<T | nul
     }
 };
   
+/**
+ * Fetches all results from an SQL query.
+ *
+ * @template T - The expected return type of the fetched data.
+ * @param {string} query - The SQL query to execute.
+ * @param {any[]} [params=[]] - The parameters for the query (optional).
+ * @returns {Promise<T[]>} A promise that resolves to an array of results, or an empty array if an error occurs.
+ */
 const fetchAll = async <T>(query: string, params: any[] = []): Promise<T[]> => {
     const db = await getDb();
     try {
