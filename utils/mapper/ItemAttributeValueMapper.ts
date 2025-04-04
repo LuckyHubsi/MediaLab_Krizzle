0625e02d1ccd346578adcade4133dacc19a5120b
seoyangjadu <plum.blossom.haejin@gmail.com>
2025-04-04 17:15:23 +0200

    feat(component): ItemAttributeValueMapper
    mapper for item attribute value

---
+++ utils/mapper/ItemAttributeValueMapper.ts
@@ -0,0 +1,22 @@
+import { ItemAttributeValueModel } from '@/models/ItemAttributeValueModel';
+import { ItemAttributeValueDTO } from '@/dto/ItemAttributeValueDTO';
+
+export class ItemAttributeValueMapper {
+    static toDTO(model: ItemAttributeValueModel): ItemAttributeValueDTO {
+        return {
+            valueID: model.valueID,
+            itemID: model.itemID,
+            attributeID: model.attributeID,
+            value: model.value
+        };
+    }
+
+    static toModel(dto: ItemAttributeValueDTO): ItemAttributeValueModel {
+        return new ItemAttributeValueModel(
+            dto.itemID,
+            dto.attributeID,
+            dto.value,
+            dto.valueID
+        );
+    }
+}
