4b8bea1e85fb2e3a2a802bcb272854402fa6d877
seoyangjadu <plum.blossom.haejin@gmail.com>
2025-04-04 17:05:38 +0200

    refactor(Model): changed CollectionModel
    Refactored CollectionModel

--- models/CollectionModel.ts
+++ models/CollectionModel.ts
@@ -1,11 +1,40 @@
-import {ItemTemplate} from './ItemTemplateModel'
-
-export interface Collection {
-    collectionID: number;
+/**
+ * Represents a Collection Entity Model used for database interactions.
+ */
+export class CollectionModel {
+    readonly collectionID?: number;
     pageID: number;
-    itemTemplate: ItemTemplate;
-}
+    itemTemplateID: number;
 
-export class Collection {
-    constructor(public collectionID: number, public pageID: number, public itemTemplate: ItemTemplate) {}
+    constructor(
+        pageID: number,
+        itemTemplateID: number,
+        collectionID?: number
+    ) {
+        this.collectionID = collectionID;
+        this.pageID = pageID;
+        this.itemTemplateID = itemTemplateID;
+    }
+    
+    // Getter methods
+    getCollectionID(): number | undefined {
+        return this.collectionID;
+    }
+    
+    getPageID(): number {
+        return this.pageID;
+    }
+    
+    getItemTemplateID(): number {
+        return this.itemTemplateID;
+    }
+    
+    // Setter methods
+    setPageID(pageID: number): void {
+        this.pageID = pageID;
+    }
+    
+    setItemTemplateID(itemTemplateID: number): void {
+        this.itemTemplateID = itemTemplateID;
+    }
 }
