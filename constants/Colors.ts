c1f06a8c7d6861b629b3c210515b5d9db300461a
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-04-04 12:41:43 +0200

    style(colors): add widget colors
    Add all widget colors to the color constants file

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -28,4 +28,96 @@ export const Colors = {
     searchBarBackground: "#2A2A2A",
     searchBarPlaceholder: "#9BA1A6",
   },
+  widget: {
+    white: "#ffffff",
+    black: "#111111",
+    grey: "#585858",
+    lightGrey: "#ABABAB",
+    blueGrey: "#82A9CC",
+    lightBlue: "#7DB5EA",
+    blue: "#4599E8",
+    darkBlue: "#1D7ED7",
+    purple: "#6D2EFF",
+    violet: "#8559ED",
+    pink: "#D50BBA",
+    rose: "#ED59C8",
+    darkRed: "#E71341",
+    red: "#FF5667",
+    green: "#118845",
+    sage: "#49976B",
+    gradientGreen: `
+    linear-gradient(
+      to bottom right, rgba(52, 61, 232, 0.00) 7%, #1CA870 50%
+    ) bottom right / 50% 50% no-repeat,
+    linear-gradient(
+      to bottom left, rgba(52, 61, 232, 0.00) 7%, #1CA870 50%
+    ) bottom left / 50% 50% no-repeat,
+    linear-gradient(
+      to top left, rgba(52, 61, 232, 0.00) 7%, #1CA870 50%
+    ) top left / 50% 50% no-repeat,
+    linear-gradient(
+      to top right, rgba(52, 61, 232, 0.00) 7%, #1CA870 50%
+    ) top right / 50% 50% no-repeat,
+    #4AE7A8
+  `,
+    GradientPurple: `
+    linear-gradient(
+      to bottom right, rgba(232, 69, 210, 0.00) 14%, #664DBD 100%
+    ) bottom right / 50% 50% no-repeat,
+    linear-gradient(
+      to bottom left, rgba(232, 69, 210, 0.00) 14%, #664DBD 100%
+    ) bottom left / 50% 50% no-repeat,
+    linear-gradient(
+      to top left, rgba(232, 69, 210, 0.00) 14%, #664DBD 100%
+    ) top left / 50% 50% no-repeat,
+    linear-gradient(
+      to top right, rgba(232, 69, 210, 0.00) 14%, #664DBD 100%
+    ) top right / 50% 50% no-repeat,
+    #7645E8
+  `,
+    gradientPink: `linear-gradient(
+      to bottom right, #F46D6F 14%, #B81CA3 100%
+    ) bottom right / 50% 50% no-repeat,
+    linear-gradient(
+      to bottom left, #F46D6F 14%, #B81CA3 100%
+    ) bottom left / 50% 50% no-repeat,
+    linear-gradient(
+      to top left, #F46D6F 14%, #B81CA3 100%
+    ) top left / 50% 50% no-repeat,
+    linear-gradient(
+      to top right, #F46D6F 14%, #B81CA3 100%
+    ) top right / 50% 50% no-repeat,
+    #F46D6F
+  `,
+    gradientRed: `
+    linear-gradient(
+      to bottom right, #FA995D 14%, #E92529 100%
+    ) bottom right / 50% 50% no-repeat,
+    linear-gradient(
+      to bottom left, #FA995D 14%, #E92529 100%
+    ) bottom left / 50% 50% no-repeat,
+    linear-gradient(
+      to top left, #FA995D 14%, #E92529 100%
+    ) top left / 50% 50% no-repeat,
+    linear-gradient(
+      to top right, #FA995D 14%, #E92529 100%
+    ) top right / 50% 50% no-repeat,
+    #FF7D99
+  `,
+    gradientBlue: `
+    linear-gradient(
+      to bottom right, rgba(69, 153, 232, 0.00) 14%, #583FE7 100%
+    ) bottom right / 50% 50% no-repeat,
+    linear-gradient(
+      to bottom left, rgba(69, 153, 232, 0.00) 14%, #583FE7 100%
+    ) bottom left / 50% 50% no-repeat,
+    linear-gradient(
+      to top left, rgba(69, 153, 232, 0.00) 14%, #583FE7 100%
+    ) top left / 50% 50% no-repeat,
+    linear-gradient(
+      to top right, rgba(69, 153, 232, 0.00) 14%, #583FE7 100%
+    ) top right / 50% 50% no-repeat,
+    #4599E8
+  `,
+  },
 };
