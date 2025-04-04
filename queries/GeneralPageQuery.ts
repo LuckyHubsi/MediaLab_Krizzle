9fcb00b88c933d89d7d3d0804e7c9a1e5a721a50
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-04 10:13:44 +0200

    feat(query): add query for getting note by pageID
    Add select query string for getting a note and its general data by pageID.
    
    Refs: SCRUM-37

--- queries/GeneralPageQuery.ts
+++ queries/GeneralPageQuery.ts
@@ -6,7 +6,30 @@ const insertNewPageQuery: string = `
     INSERT INTO general_page_data (page_type, page_title, page_icon, page_color, date_created, date_modified, archived, pinned) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 `;
 
+const selectNoteByPageIDQuery: string = `
+   SELECT
+        p.pageID AS pageID,
+        p.page_type,
+        p.page_title,
+        p.page_icon,
+        p.page_color,
+        p.date_created,
+        p.date_modified,
+        p.archived,
+        p.pinned,
+        n.noteID,
+        n.note_content
+    FROM
+        general_page_data p
+    INNER JOIN
+        note n ON p.pageID = n.pageID
+    WHERE
+        p.pageID = ?;
+`;
+
+
 export {
     selectAllGeneralPageQuery,
-    insertNewPageQuery
+    insertNewPageQuery,
+    selectNoteByPageIDQuery
 }
