b4dba4fa968f1a6e193cf70aaf43fe7285c636a9
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-04 08:32:44 +0200

    feat(entity model): refactor note entity model
    Refactored NoteModel to follow changed backend conventions.
    
    Refs: SCRUM-5

--- models/NoteModel.ts
+++ models/NoteModel.ts
@@ -1,9 +1,19 @@
-export interface Note {
-    noteID: number;
-    noteContent: string | null;
+/**
+ * Represents a Note Entity Model used for database interactions.
+ */
+export class NoteModel {
+    noteID?: number;
+    note_content: string | null;
     pageID: number;
-}
 
-export class Note {
-    constructor(public noteID: number, public noteContent: string | null, public pageID: number) {}
+
+    constructor(
+        note_content: string | null,
+        pageID: number,
+        noteID?: number
+    ) {
+        this.noteID = noteID;
+        this.note_content = note_content;
+        this.pageID = pageID;
+    }
 }
