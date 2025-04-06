be4af58d605f45cfdda2e38e3f8e361211353642
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-06 19:22:32 +0200

    feat(service): add updateNoteContent() function
    Add function to update text note contents by noteID.
    
    Refs: SCRUM-12

--- services/NoteService.ts
+++ services/NoteService.ts
@@ -1,9 +1,9 @@
-import { insertNoteQuery } from "@/queries/NoteQuery";
+import { insertNoteQuery, selectPageIDByNoteIDQuery, updateNoteContentQuery } from "@/queries/NoteQuery";
 import { insertGeneralPageAndReturnID } from "./GeneralPageService";
 import { executeQuery, fetchFirst } from "@/utils/QueryHelper";
 import { NoteDTO } from "@/dto/NoteDTO";
 import { NoteMapper } from "@/utils/mapper/NoteMapper";
-import { selectNoteByPageIDQuery } from "@/queries/GeneralPageQuery";
+import { selectNoteByPageIDQuery, updateDateModifiedByPageIDQuery } from "@/queries/GeneralPageQuery";
 import { NoteModel } from "@/models/NoteModel";
 import { PageType } from "@/utils/enums/PageType";
 
@@ -48,7 +48,53 @@ const getNoteDataByPageID = async (pageID: number): Promise<NoteDTO | null> => {
     return null;
 }
 
+/**
+ * Updates the content of a note and updates the corresponding page's `date_modified` timestamp.
+ *
+ * @param noteID - The unique identifier of the note to update.
+ * @param newNoteContent - The new content to save for the note.
+ * @returns A Promise that resolves to `true` if the update was successful, or `false` if an error occurred.
+ */
+const updateNoteContent = async (noteID: number, newNoteContent: string): Promise<boolean> => {
+    try {
+        // get the pageID from the noteID
+        const pageID = await getPageIDForNote(noteID);
+        if (!pageID) {
+            console.error("Page ID not found for the given note ID");
+            return false;
+        }
+
+        // update the note content
+        await executeQuery(updateNoteContentQuery, [noteID, newNoteContent]);
+
+        // update the date_modified for the general page data
+        await executeQuery(updateDateModifiedByPageIDQuery, [new Date().toISOString, pageID]);
+
+        return true;
+    } catch (error) {
+        console.error("Error updating note content:", error);
+        return false;
+    }
+};
+
+/**
+ * Retrieves the page ID associated with a given note ID.
+ *
+ * @param noteID - The unique identifier of the note.
+ * @returns A Promise that resolves to the page ID if found, or `null` if not found or an error occurred.
+ */
+const getPageIDForNote = async (noteID: number): Promise<number | null> => {
+    try {
+        const result = await fetchFirst<{ pageID: number }>(selectPageIDByNoteIDQuery, [noteID]);
+        return result?.pageID ?? null;
+    } catch (error) {
+        console.error("Error fetching pageID for note:", error);
+        return null;
+    }
+};
+
 export {
     insertNote,
     getNoteDataByPageID,
+    updateNoteContent
 }
