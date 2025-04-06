3443bd1b3381ba92c059d94097acef10c57c904d
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-06 19:10:17 +0200

    refactor(service): modify insertNote()
    Pass NoteDTO as param instead of GeneralPageDTO to the insertNote() function.
    
    Refs: SCRUM-4

--- services/NoteService.ts
+++ services/NoteService.ts
@@ -1,7 +1,7 @@
 import { insertNoteQuery } from "@/queries/NoteQuery";
 import { insertGeneralPageAndReturnID } from "./GeneralPageService";
-import { GeneralPageDTO } from "@/dto/GeneralPageDTO";
-import { executeQuery } from "@/utils/QueryHelper";
+import { executeQuery, fetchFirst } from "@/utils/QueryHelper";
+import { NoteDTO } from "@/dto/NoteDTO";
 
 /**
  * Inserts a new note into the database.
@@ -9,13 +9,13 @@ import { executeQuery } from "@/utils/QueryHelper";
  * @param {NoteDTO} NoteDTO - The DTO representing the note to insert.
  * @returns {Promise<number | null>} A promise that resolves to the inserted note's ID, or null if the insertion fails.
  */
-const insertNote = async (generalPageDTO: GeneralPageDTO): Promise<number | null> => {
+const insertNote = async (noteDTO: NoteDTO): Promise<number | null> => {
     try {
         // first inserts the general page data and returns the pageID
-        const pageID = await insertGeneralPageAndReturnID(generalPageDTO);
+        const pageID = await insertGeneralPageAndReturnID(noteDTO);
 
         await executeQuery(insertNoteQuery, [
-            null,
+            noteDTO.note_content,
             pageID
         ]);
 
