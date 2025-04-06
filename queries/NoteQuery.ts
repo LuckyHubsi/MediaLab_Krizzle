42c028bbd14c50bf01f97d33565ba7c732ee49c1
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-06 14:51:47 +0200

    feat(query): add update note content query string
    Add query string for updating textual note content.
    
    Refs: SCRUM-13

--- queries/NoteQuery.ts
+++ queries/NoteQuery.ts
@@ -2,6 +2,11 @@ const insertNoteQuery: string = `
     INSERT INTO note (note_content, pageID) VALUES (?, ?)
 `;
 
+const updateNoteContentQuery: string = `
+    UPDATE note SET note_content = ? WHERE noteID = ?
+`;
+
 export {
-    insertNoteQuery
+    insertNoteQuery,
+    updateNoteContentQuery
 }
