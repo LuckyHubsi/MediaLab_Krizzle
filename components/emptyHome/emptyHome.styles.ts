bba2f87a29bedd126afdd3b71144bb49251daac1
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-04-06 13:24:31 +0200

    fix(EmptyHome): fix EmptyHome styles
    Adapt margin-top of the Empty Home styles, so the component is in the middle

--- components/emptyHome/emptyHome.styles.ts
+++ components/emptyHome/emptyHome.styles.ts
@@ -3,7 +3,7 @@ import { Colors } from "@/constants/Colors";
 
 export const StyledEmptyHome = styled.View`
   flex: 1;
-  margin-top: 30%;
+  margin-top: 45%;
   align-items: center;
   padding: 20px;
   height: 100%;
