9aea79a56aedb78ca3b354d57cfcd6fca95d03aa
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-23 16:18:19 +0200

    refactor(insertAttribute): return Promise<void>
    Refactor to return Promise<void> instead of ID as attribute ids are not necessary to retrieve during insertion.
    
    Refs: SCRUM-43

--- queries/AttributeQuery.ts
+++ queries/AttributeQuery.ts
@@ -1,7 +1,5 @@
-const insertAttribute: string = `
+const insertAttributeQuery: string = `
     INSERT INTO attribute (attribute_label, type, preview, item_templateID) VALUES (?, ?, ?, ?)
 `;
 
-export {
-    insertAttribute
-}
+export { insertAttributeQuery };
--- services/AttributeService.ts
+++ services/AttributeService.ts
@@ -1,44 +1,30 @@
-import { AttributeDTO, isValidAttributeType } from '@/dto/AttributeDTO';
-import { AttributeType } from '@/utils/enums/AttributeType';
-import { insertAttribute } from '@/queries/AttributeQuery';
-import { executeQuery, getLastInsertId } from '@/utils/QueryHelper';
+import { AttributeDTO, isValidAttributeType } from "@/dto/AttributeDTO";
+import { AttributeType } from "@/utils/enums/AttributeType";
+import { insertAttributeQuery } from "@/queries/AttributeQuery";
+import { executeQuery, getLastInsertId } from "@/utils/QueryHelper";
 
 /**
  * Inserts a new attribute into the database and returns its ID.
- * 
+ *
  * @param {AttributeDTO} attributeDTO - The DTO representing the attribute to insert.
- * @returns {Promise<number | null>} A promise that resolves to the inserted attribute's ID, or null if the insertion fails.
+ * @returns {Promise<void>} A promise that resolves to the inserted attribute's ID, or null if the insertion fails.
  */
-const insertAttributeAndReturnID = async (attributeDTO: AttributeDTO): Promise<number | null> => {
-    try {
-        // Validate attribute type or use default
-        if (!isValidAttributeType(attributeDTO.attributeType)) {
-            attributeDTO.attributeType = AttributeType.Text;
-        }
-
-        await executeQuery(insertAttribute, [
-            attributeDTO.itemTemplateID,
-            attributeDTO.attributeLabel,
-            attributeDTO.attributeType,
-            attributeDTO.preview ? 1 : 0
-        ]);
-
-        // get inserted attribute ID
-        const result = await getLastInsertId();
-
-        if (result) {
-            console.log("Inserted Attribute ID:", result);
-            return result;
-        } else {
-            console.error("Failed to fetch inserted attribute ID");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error inserting attribute:", error);
-        return null;
+const insertAttribute = async (attributeDTO: AttributeDTO): Promise<void> => {
+  try {
+    // Validate attribute type or use default
+    if (!isValidAttributeType(attributeDTO.attributeType)) {
+      attributeDTO.attributeType = AttributeType.Text;
     }
+
+    await executeQuery(insertAttributeQuery, [
+      attributeDTO.itemTemplateID,
+      attributeDTO.attributeLabel,
+      attributeDTO.attributeType,
+      attributeDTO.preview ? 1 : 0,
+    ]);
+  } catch (error) {
+    console.error("Error inserting attribute:", error);
+  }
 };
 
-export {
-    insertAttributeAndReturnID
-}
+export { insertAttribute };
