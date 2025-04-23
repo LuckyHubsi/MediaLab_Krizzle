eead967fc7c0b992c30e722fc2201bbd6bfa94ca
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-23 16:01:08 +0200

    refactor(insertItemTemplateAndReturnID): use executeTransaction
    Use executeTransaction to rollback the transaction in case one step fails.
    
    Refs: SCRUM-42

--- services/ItemTemplateService.ts
+++ services/ItemTemplateService.ts
@@ -1,42 +1,41 @@
-import { ItemTemplateDTO } from '@/dto/ItemTemplateDTO';
-import { insertItemTemplate } from '@/queries/ItemTemplateQuery';
+import { ItemTemplateDTO } from "@/dto/ItemTemplateDTO";
+import { insertItemTemplate } from "@/queries/ItemTemplateQuery";
 import {
-    executeQuery,
-    getLastInsertId
-} from '@/utils/QueryHelper';
-
+  executeQuery,
+  executeTransaction,
+  getLastInsertId,
+} from "@/utils/QueryHelper";
 
 /**
  * Inserts a new item template into the database and returns its ID.
- * 
+ *
  * @param {ItemTemplateDTO} itemTemplateDTO - The DTO representing the item template to insert.
  * @returns {Promise<number | null>} A promise that resolves to the inserted item template's ID, or null if the insertion fails.
  */
-const insertItemTemplateAndReturnID = async (itemTemplateDTO: ItemTemplateDTO): Promise<number | null> => {
-    try {
+const insertItemTemplateAndReturnID = async (
+  itemTemplateDTO: ItemTemplateDTO,
+): Promise<number | null> => {
+  try {
+    const templateID: number | null = await executeTransaction<number | null>(
+      async () => {
+        await executeQuery(insertItemTemplate, [itemTemplateDTO.template_name]);
 
-        await executeQuery(insertItemTemplate, [
-            itemTemplateDTO.template_name,
-            itemTemplateDTO.categories
-        ]);
+        // get inserted page ID
+        const lastInsertedID = await getLastInsertId();
+        return lastInsertedID;
+      },
+    );
 
-        // get inserted item template ID
-        const result = await getLastInsertId();
-
-        if (result) {
-            console.log("Inserted Item Template ID:", result);
-            return result;
-        } else {
-            console.error("Failed to fetch inserted item template ID");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error inserting item template:", error);
-        return null;
+    if (templateID) {
+      return templateID;
+    } else {
+      console.error("Failed to fetch inserted item template ID");
+      return null;
     }
+  } catch (error) {
+    console.error("Error inserting item template:", error);
+    return null;
+  }
 };
 
-
-export {
-    insertItemTemplateAndReturnID,
-}
+export { insertItemTemplateAndReturnID };
