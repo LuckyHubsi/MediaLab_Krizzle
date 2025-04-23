6f0b9984bb269fcf88edd6aa7c341579552cc8a6
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-23 20:27:35 +0200

    refactor(AttributeDTO): pass options as array
    Pass options as an array instead of a single string.
    
    Refs: SCRUM-72

--- dto/AttributeDTO.ts
+++ dto/AttributeDTO.ts
@@ -1,20 +1,20 @@
-import { AttributeType } from '@/utils/enums/AttributeType';
+import { AttributeType } from "@/utils/enums/AttributeType";
 
 /**
  * Represents an Attribute data structure
  */
 export type AttributeDTO = {
-    attributeID?: number;
-    itemTemplateID?: number;
-    attributeLabel: string;
-    attributeType: AttributeType; // One of: "Text", "Rating", "Date", "Multiselect"
-    preview: boolean;
-    options?: string | null; // For storing options for Multiselect type or other settings
-}
+  attributeID?: number;
+  itemTemplateID?: number;
+  attributeLabel: string;
+  attributeType: AttributeType; // One of: "Text", "Rating", "Date", "Multiselect"
+  preview: boolean;
+  options?: string[] | null; // For storing options for Multiselect type or other settings
+};
 
 /**
  * Type guard to validate attribute type
  */
 export function isValidAttributeType(type: AttributeType): boolean {
-    return ["text", "rating", "date", "multiselect"].includes(type);
+  return ["text", "rating", "date", "multiselect"].includes(type);
 }
