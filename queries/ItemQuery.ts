555d328a41b058349fa6b8783cabe84455bea919
seoyangjadu <plum.blossom.haejin@gmail.com>
2025-04-25 10:34:56 +0200

    feat(itemSelectByCollectionIdQuery): wrote the select query for all items in a collection
    similar to single item query, but takes into account which attributes are in the preview
    
    Refs: SCRUM-47

--- queries/ItemQuery.ts
+++ queries/ItemQuery.ts
@@ -53,10 +53,69 @@ function itemSelectByIdQuery(itemId: number): string {
   `;
 }
 
+/*
+
 // TODO: change to function to dynamically build query string based on an array of attributes that are set to preview
 const itemSelectByCollectionIdQuery: string = `
     SELECT * FROM items WHERE collectionID = ?
 `;
+
+*/
+
+/**
+ * Builds a query to fetch all items in a collection
+ * @param {number} collectionId - The ID of the collection
+ * @returns {string} - The SQL query
+ */
+function itemSelectByCollectionIdQuery(collectionId: number): string {
+    return `
+        SELECT 
+        i.itemID,
+        i.collectionID,
+        i.category,
+        json_group_array(
+            json_object(
+                'attributeID', a.attributeID,
+                'attributeLabel', a.attribute_label,
+                'attributeType', a.type,
+                'value', CASE 
+                    WHEN a.type = 'text' THEN t.value
+                    WHEN a.type = 'date' THEN d.value
+                    WHEN a.type = 'rating' THEN r.value
+                    WHEN a.type = 'multiselect' THEN (
+                        SELECT json_group_array(
+                            json_object('multiselectID', mo.multiselectID, 'options', mo.options)
+                        )
+                        FROM multiselect ms
+                        JOIN multiselect_options mo ON mo.multiselectID = ms.multiselectID
+                        WHERE ms.attributeID = a.attributeID AND ms.itemID = i.itemID
+                    )
+                    ELSE NULL
+                END,
+                'options', CASE 
+                    WHEN a.type = 'multiselect' THEN (
+                        SELECT json_group_array(
+                            json_object('multiselectID', mo.multiselectID, 'options', mo.options)
+                        )
+                        FROM multiselect_options mo
+                        WHERE mo.attributeID = a.attributeID
+                    )
+                    ELSE NULL
+                END
+            )
+        ) AS attributeValues
+    FROM item i
+    JOIN collection c ON i.collectionID = c.collectionID
+    JOIN item_template it ON c.item_templateID = it.item_templateID
+    JOIN attribute a ON it.item_templateID = a.item_templateID
+    LEFT JOIN collection_category cc on i.category = cc.category_name
+    LEFT JOIN text t ON t.attributeID = a.attributeID AND t.itemID = i.itemID
+    LEFT JOIN date d ON d.attributeID = a.attributeID AND d.itemID = i.itemID
+    LEFT JOIN rating r ON r.attributeID = a.attributeID AND r.itemID = i.itemID
+    WHERE i.collectionID = ? AND a.preview = true
+    GROUP BY i.itemID
+  `;
+}
 
 const insertItem: string = `
     INSERT INTO items (collectionID, pageID, category) 
