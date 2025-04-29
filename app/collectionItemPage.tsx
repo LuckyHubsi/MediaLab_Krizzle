b922837be420c9f28c56f26a803554fa9bf189cf
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-29 09:28:12 +0200

    fix(collectionItemPage): change params
    Change param and comment out components.
    
    Refs: SCRUM-17

--- app/collectionItemPage.tsx
+++ app/collectionItemPage.tsx
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemedView } from "@/components/ui/ThemedView/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CustomStyledHeader } from "@/components/ui/CustomStyledHeader/CustomStyledHeader";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { CollectionLoadItem } from "@/components/ui/CollectionLoadItems/CollectionLoadItems";
 import { ScrollView } from "react-native"; // Use ScrollView from react-native
+import { getItemById } from "@/services/ItemService";
+import { ItemDTO } from "@/dto/ItemDTO";
 
 export default function CollectionItemScreen() {
-  const { collectionData } = useLocalSearchParams<{
-    collectionData: string;
+  const { itemId } = useLocalSearchParams<{
+    itemId: string;
   }>();
 
-  const parsedCollectionData = collectionData
-    ? JSON.parse(collectionData)
-    : null;
+  const [item, setItem] = useState<ItemDTO>();
 
-  //const navigation = useNavigation();
+  useEffect(() => {
+    (async () => {
+      const numericItemId = Number(itemId);
+      const item = await getItemById(numericItemId);
+      setItem(item);
+      console.log(JSON.stringify(item, null, 2));
+    })();
+  }, [itemId]);
 
   return (
     <>
       <SafeAreaView style={{ flex: 1 }}>
         <CustomStyledHeader
-          title={
-            parsedCollectionData?.collectionTitleValue || "Collection Item"
-          } //Here should be the title of the collection
-          backBehavior="goHome" // Go back to home when back button is pressed
+          title={item?.page_title || "Collection Item"} //Here should be the title of the collection
+          backBehavior="default" // Go back to home when back button is pressed
           iconName={undefined} // No icon for the header
           onIconPress={() => {}} // No action when pressed
           iconName2="more-horiz" // icon for the pop up menu
@@ -35,7 +40,7 @@ export default function CollectionItemScreen() {
           showsVerticalScrollIndicator={false} // Hides the vertical scrollbar
         >
           <ThemedView topPadding={0}>
-            <CollectionLoadItem
+            {/* <CollectionLoadItem
               collectionTitle={
                 parsedCollectionData?.collectionTitle || "Default Title"
               }
@@ -62,7 +67,7 @@ export default function CollectionItemScreen() {
               collectionSelectableTitle={
                 parsedCollectionData?.CollectionSelectableTitle || "Selectable"
               }
-            />
+            /> */}
           </ThemedView>
         </ScrollView>
       </SafeAreaView>
