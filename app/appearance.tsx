42a686ba0ac25f10b2d8f9b3886b1821a4e3289e
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-04-29 10:33:24 +0200

    style(ThemeSelector): Add ThemeSelector component
    Add ThemeSelector component

--- app/appearance.tsx
+++ app/appearance.tsx
@@ -5,15 +5,29 @@ import { SettingsLink } from "@/components/ui/SettingsLink/SettingsLink";
 import { Header } from "@/components/ui/Header/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CustomStyledHeader } from "@/components/ui/CustomStyledHeader/CustomStyledHeader";
+import { Card } from "@/components/ui/Card/Card";
+import { ThemeSelector } from "@/components/ui/ThemeSelector/ThemeSelector";
+import { useState } from "react";
+import { useUserTheme } from "@/context/ThemeContext";
+import { useColorScheme } from "react-native";
 
 export default function AppearanceScreen() {
+  const { userTheme, setUserTheme } = useUserTheme();
+  const systemColorScheme = useColorScheme() ?? "light";
+  const selectedTheme = userTheme === "system" ? systemColorScheme : userTheme;
   return (
     <SafeAreaView>
       <CustomStyledHeader title={"Appearance"} iconName="more-horiz" />
       <ThemedView>
         <ThemedText fontSize="regular" fontWeight="regular">
-          Appearance
+          Light/Dark Themes
         </ThemedText>
+        <Card>
+          <ThemeSelector
+            selected={selectedTheme}
+            onSelect={(theme) => setUserTheme(theme)}
+          />
+        </Card>
       </ThemedView>
     </SafeAreaView>
   );
