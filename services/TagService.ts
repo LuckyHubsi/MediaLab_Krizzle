ed01201dfa990523a8f7f9e7c031308b0bd8f805
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-04-30 18:35:15 +0200

    feat(insertTag): add functionality
    Add service function to insert a tag with a tag_label.
    
    Refs: SCRUM-79

--- services/TagService.ts
+++ services/TagService.ts
@@ -1,9 +1,10 @@
-import { fetchAll } from "@/utils/QueryHelper";
-import { selectAllTagsQuery } from "@/queries/TagQuery";
+import { executeQuery, fetchAll } from "@/utils/QueryHelper";
+import { insertTagQuery, selectAllTagsQuery } from "@/queries/TagQuery";
 import { TagModel } from "@/models/TagModel";
 import { TagDTO } from "@/dto/TagDTO";
 import { TagMapper } from "@/utils/mapper/TagMapper";
 import * as SQLite from "expo-sqlite";
+import { DatabaseError } from "@/utils/DatabaseError";
 
 /**
  * Retrieves all tags from the database.
@@ -15,4 +16,16 @@ export const getAllTags = async (
 ): Promise<TagDTO[]> => {
   const rawTags = await fetchAll<TagModel>(selectAllTagsQuery, [], txn);
   return rawTags.map(TagMapper.toDTO);
+};
+
+export const insertTag = async (
+  tag: TagDTO,
+  txn?: SQLite.SQLiteDatabase,
+): Promise<boolean> => {
+  try {
+    await executeQuery(insertTagQuery, [tag.tag_label], txn);
+    return true;
+  } catch (error) {
+    throw new DatabaseError("Failed to insert add new tag.");
+  }
 };
