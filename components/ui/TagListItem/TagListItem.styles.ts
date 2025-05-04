111f3aff039c131da3e701f55828122a4625fbc6
Marco Huber <cc221040@fhstp.ac.at>
2025-05-04 23:27:44 +0200

    style(TagListItem): change style
    Change style to look like the design in figma.
    
    Refs: #SCRUM-174

--- components/ui/TagListItem/TagListItem.styles.ts
+++ components/ui/TagListItem/TagListItem.styles.ts
@@ -4,16 +4,7 @@ export const Row = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 12px 0;
-  border-bottom-width: 1px;
-  border-bottom-color: #ccc;
-`;
-
-export const TagText = styled.Text<{ colorScheme: "light" | "dark" }>`
-  font-family: Lexend;
-  font-size: 16px;
-  color: ${({ colorScheme }: { colorScheme: "light" | "dark" }) =>
-    colorScheme === "light" ? "#000" : "#fff"};
+  padding: 10px 0;
 `;
 
 export const IconRow = styled.View`
--- components/ui/TagListItem/TagListItem.tsx
+++ components/ui/TagListItem/TagListItem.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
-import { Row, TagText, IconRow } from "./TagListItem.styles";
+import { Row, IconRow } from "./TagListItem.styles";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useActiveColorScheme } from "@/context/ThemeContext";
+import { ThemedText } from "@/components/ThemedText";
 
 interface Props {
   tag: string;
@@ -15,19 +16,19 @@ export const TagListItem: React.FC<Props> = ({ tag, onEdit, onDelete }) => {
 
   return (
     <Row>
-      <TagText colorScheme={colorScheme}>{tag}</TagText>
+      <ThemedText>{tag}</ThemedText>
       <IconRow>
         <TouchableOpacity onPress={onEdit} style={{ marginRight: 16 }}>
           <MaterialIcons
             name="edit"
-            size={20}
+            size={24}
             color={colorScheme === "light" ? "#000" : "#fff"}
           />
         </TouchableOpacity>
         <TouchableOpacity onPress={onDelete}>
           <MaterialIcons
             name="close"
-            size={20}
+            size={24}
             color={colorScheme === "light" ? "#d00" : "#f66"}
           />
         </TouchableOpacity>
