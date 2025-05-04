98883606dbf3cbe8b80a05cd7941592236307bf3
Marco Huber <cc221040@fhstp.ac.at>
2025-05-04 23:26:06 +0200

    style(tagList): add icon and onPress
    Change style to allow new icon next to selected tag. Add onPress function to go to tagManagement page.
    
    Refs: #SCRUM-100

--- components/ui/TagList/TagList.tsx
+++ components/ui/TagList/TagList.tsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { ScrollView, View } from "react-native";
 import { TagButton, TagText } from "./TagList.styles";
 import { useActiveColorScheme } from "@/context/ThemeContext";
+import { MaterialIcons } from "@expo/vector-icons";
 
 type TagListProps = {
   tags: string[];
   onSelect?: (selected: string) => void;
+  onPress?: () => void;
 };
 
-const TagList: React.FC<TagListProps> = ({ tags, onSelect }) => {
+const TagList: React.FC<TagListProps> = ({ tags, onSelect, onPress }) => {
   const [activeTag, setActiveTag] = useState(tags[0]);
   const themeMode = useActiveColorScheme() ?? "light";
 
@@ -27,11 +29,30 @@ const TagList: React.FC<TagListProps> = ({ tags, onSelect }) => {
             themeMode={themeMode}
             onPress={() => handlePress(tag)}
           >
+            {tag === activeTag && (
+              <MaterialIcons
+                name="check-circle"
+                size={16}
+                color="#FBFBFB"
+                style={{ marginRight: 5 }}
+              />
+            )}
             <TagText active={tag === activeTag} themeMode={themeMode}>
               {tag}
             </TagText>
           </TagButton>
         ))}
+        {tags.length === 1 && (
+          <TagButton themeMode={themeMode} onPress={onPress}>
+            <MaterialIcons
+              name="add"
+              size={16}
+              color={themeMode === "dark" ? "#FBFBFB" : "#000"}
+              style={{ marginRight: 5 }}
+            />
+            <TagText themeMode={themeMode}>Add a tag</TagText>
+          </TagButton>
+        )}
       </ScrollView>
     </View>
   );
