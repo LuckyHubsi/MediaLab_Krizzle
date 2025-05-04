1de89f2b0a439494864506552d0e7ffa2153147c
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-04 22:24:18 +0200

    refactor(GeneralPageQuery): duplicate file
    Duplicate file into backend folders. Leave old one inside the projec to not break working app version.
    
    Refs: SCRUM-133

---
+++ backend/repository/query/GeneralPageQuery.ts
@@ -0,0 +1,34 @@
+const selectAllGeneralPageQuery: string = `
+    SELECT p.*, t.tag_label
+    FROM general_page_data p
+    LEFT JOIN tag t ON p.tagID = t.tagID
+    WHERE p.archived = 0
+    ORDER BY p.date_modified DESC;
+`;
+
+const insertNewPageQuery: string = `
+    INSERT INTO general_page_data (page_type, page_title, page_icon, page_color, date_created, date_modified, archived, pinned) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+`;
+
+const selectNoteByPageIDQuery: string = `
+    SELECT p.*, n.note_content
+    FROM general_page_data p
+    INNER JOIN note n ON p.pageID = n.pageID
+    WHERE p.pageID = ?;
+`;
+
+const updateDateModifiedByPageIDQuery: string = `
+    UPDATE general_page_data SET date_modified = ? WHERE pageID = ?
+`;
+
+const deleteGeneralPageByIDQuery: string = `
+    DELETE FROM general_page_data WHERE pageID = ?
+`;
+
+export {
+  selectAllGeneralPageQuery,
+  insertNewPageQuery,
+  selectNoteByPageIDQuery,
+  updateDateModifiedByPageIDQuery,
+  deleteGeneralPageByIDQuery,
+};
