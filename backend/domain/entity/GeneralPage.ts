b818438d8538bdfecd6b1a414578bd6af162028a
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-04 22:19:06 +0200

    feat(GeneralPage): add domain entity
    Add domain entity with zod.
    
    Refs: SCRUM-133

---
+++ backend/domain/entity/GeneralPage.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import * as common from "../common/types";
+import { tagSchema } from "./Tag";
+import { PageType } from "@/backend/util/enum/PageType";
+
+export const pageID = common.positiveInt.brand<"PageId">();
+export type PageID = z.infer<typeof pageID>;
+
+export const generalPageSchema = z.object({
+  pageID: pageID,
+  pageType: z.nativeEnum(PageType),
+  pageTitle: common.string30,
+  pageIcon: common.optionalNullableString,
+  pageColor: common.optionalNullableString,
+  archived: common.boolean,
+  pinned: common.boolean,
+  tag: tagSchema.nullable().optional(),
+  createdAt: common.date,
+  updatedAt: common.date,
+});
+
+export type GeneralPage = z.infer<typeof generalPageSchema>;
+
+export const createNewGeneralPage = z
+  .object({
+    pageType: z.nativeEnum(PageType),
+    pageTitle: common.string30,
+    pageIcon: common.optionalNullableString,
+    pageColor: common.optionalNullableString,
+    archived: common.boolean.default(false),
+    pinned: common.boolean.default(false),
+    tag: tagSchema.nullable().optional(),
+  })
+  .transform(
+    (data) =>
+      ({
+        ...data,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }) satisfies Omit<GeneralPage, "pageID">,
+  );
+
+export type NewGeneralPage = z.infer<typeof createNewGeneralPage>;
