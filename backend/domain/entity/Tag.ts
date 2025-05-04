e7548473f0acce3f0d792e2cad454d0bbbfe0623
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-04 20:19:42 +0200

    feat(Tag): add domain entity
    Add domain entity schema with zod.
    
    Refs: SCRUM-133

---
+++ backend/domain/entity/Tag.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import * as common from "../common/types";
+
+export const tagID = common.positiveInt.brand<"TagID">();
+export type TagID = z.infer<typeof tagID>;
+
+export const tagSchema = z.object({
+  tagID: tagID,
+  tagLabel: common.string30,
+});
+
+export type Tag = z.infer<typeof tagSchema>;
+
+export const createNewTagSchema = z.object({
+  tagLabel: common.string30,
+});
+
+export type NewTag = z.infer<typeof createNewTagSchema>;
