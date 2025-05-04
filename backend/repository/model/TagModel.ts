1149a997ad2085d0254024e97e0a78315ef2ff22
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-04 20:25:15 +0200

    refactor(TagModel): duplicate to backend
    Duplicate the folder to backend, to gradually move components and refactor.
    
    Refs: SCRUM-133

---
+++ backend/repository/model/TagModel.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents a tag used to categorize or label content.
+ *
+ * @property tagID - The unique identifier for the tag.
+ * @property tag_label - The human-readable label or name of the tag.
+ */
+export type TagModel = {
+  tagID: number;
+  tag_label: string;
+};
