411ce27344a3147cafecd4a59edee51373ff11f7
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-05 02:22:43 +0200

    feat(NoteRepository): add repo
    Add NoteRepository interface and implementation.
    
    Refs: SCRUM-133

---
+++ backend/repository/implementation/NoteRepository.implementation.ts
@@ -0,0 +1,66 @@
+import { Note } from "@/backend/domain/entity/Note";
+import { NoteRepository } from "../interfaces/NoteRepository.interface";
+import { BaseRepositoryImpl } from "./BaseRepository.implementation";
+import { NoteModel } from "../model/NoteModel";
+import {
+  selectNoteByPageIDQuery,
+  updateDateModifiedByPageIDQuery,
+} from "../query/GeneralPageQuery";
+import { PageType } from "@/backend/util/enum/PageType";
+import { NoteMapper } from "@/backend/util/mapper/NoteMapper";
+import { insertNoteQuery, updateNoteContentQuery } from "../query/NoteQuery";
+import { PageID, pageID } from "@/backend/domain/entity/GeneralPage";
+import { RepositoryError } from "@/backend/util/error/RepositoryError";
+
+export class NoteRepositoryImpl
+  extends BaseRepositoryImpl
+  implements NoteRepository
+{
+  async getByPageId(pageId: PageID): Promise<Note | null> {
+    try {
+      const noteData = await this.fetchFirst<NoteModel>(
+        selectNoteByPageIDQuery,
+        [pageId],
+      );
+      if (!noteData || noteData.page_type !== PageType.Note) {
+        return null;
+      }
+      return NoteMapper.toEntity(noteData);
+    } catch (error) {
+      throw new RepositoryError("Failed to fetch page.");
+    }
+  }
+
+  async insertNote(
+    note: Omit<Note, "pageID" | "noteID" | "createdAt" | "updatedAt">,
+    pageId: number,
+  ): Promise<number | null> {
+    try {
+      const brandedPageId = pageID.parse(pageId);
+      await this.executeQuery(insertNoteQuery, [
+        note.noteContent,
+        brandedPageId,
+      ]);
+      return pageId;
+    } catch (error) {
+      console.error("Error inserting note:", error);
+      return null;
+    }
+  }
+
+  async updateContent(pageId: PageID, newContent: string): Promise<boolean> {
+    try {
+      await this.executeQuery(updateNoteContentQuery, [newContent, pageId]);
+      await this.executeQuery(updateDateModifiedByPageIDQuery, [
+        new Date().toISOString(),
+        pageId,
+      ]);
+      return true;
+    } catch (error) {
+      console.error("Error updating note content:", error);
+      return false;
+    }
+  }
+}
+
+export const noteRepository = new NoteRepositoryImpl();
---
+++ backend/repository/interfaces/NoteRepository.interface.ts
@@ -0,0 +1,14 @@
+import { PageID } from "@/backend/domain/entity/GeneralPage";
+import { Note } from "../../domain/entity/Note";
+import { BaseRepository } from "./BaseRepository.interface";
+import * as SQLite from "expo-sqlite";
+
+export interface NoteRepository extends BaseRepository {
+  getByPageId(pageId: PageID): Promise<Note | null>;
+  insertNote(
+    note: Omit<Note, "pageID" | "noteID" | "createdAt" | "updatedAt">,
+    pageId: PageID,
+    txn?: SQLite.SQLiteDatabase,
+  ): Promise<number | null>;
+  updateContent(pageId: PageID, newContent: string): Promise<boolean>;
+}
