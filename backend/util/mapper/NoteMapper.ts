ad885e0fbd858aa121a152b4d9eb05ada9d5c46c
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-05 02:06:42 +0200

    feat(NoteMapper): add new mapper class
    Add new NoteMapper class. Leave original one inside to not break the working application.
    
    Refs: SCRUM-133

---
+++ backend/util/mapper/NoteMapper.ts
@@ -0,0 +1,110 @@
+import {
+  createNewNote,
+  NewNote,
+  Note,
+  noteID,
+  noteSchema,
+} from "@/backend/domain/entity/Note";
+import { NoteDTO } from "@/dto/NoteDTO";
+import { TagMapper } from "./TagMapper";
+import { NoteModel } from "@/backend/repository/model/NoteModel";
+import { pageID } from "@/backend/domain/entity/GeneralPage";
+
+export class NoteMapper {
+  static toDTO(entity: Note): NoteDTO {
+    return {
+      pageID: entity.pageID,
+      page_type: entity.pageType,
+      page_title: entity.pageTitle,
+      page_icon: entity.pageIcon,
+      page_color: entity.pageColor,
+      archived: entity.archived,
+      pinned: entity.pinned,
+      tag: entity.tag ? TagMapper.toDTO(entity.tag) : null,
+      noteID: entity.noteID,
+      note_content: entity.noteContent,
+    };
+  }
+
+  static toModel(entity: Note): NoteModel {
+    return {
+      pageID: entity.pageID,
+      page_type: entity.pageType,
+      page_title: entity.pageTitle,
+      page_icon: entity.pageIcon,
+      page_color: entity.pageColor,
+      date_created: entity.createdAt.toISOString(),
+      date_modified: entity.updatedAt.toISOString(),
+      archived: entity.archived ? 1 : 0,
+      pinned: entity.pinned ? 1 : 0,
+      tagID: entity.tag?.tagID ?? null,
+      tag_label: entity.tag?.tagLabel,
+      noteID: entity.noteID,
+      note_content: entity.noteContent,
+    };
+  }
+
+  static toInsertModel(entity: NewNote): NoteModel {
+    return {
+      pageID: 0,
+      page_type: entity.pageType,
+      page_title: entity.pageTitle,
+      page_icon: entity.pageIcon,
+      page_color: entity.pageColor,
+      date_created: entity.createdAt.toISOString(),
+      date_modified: entity.updatedAt.toISOString(),
+      archived: entity.archived ? 1 : 0,
+      pinned: entity.pinned ? 1 : 0,
+      tagID: entity.tag?.tagID ?? null,
+      tag_label: entity.tag?.tagLabel,
+      noteID: 0,
+      note_content: entity.noteContent,
+    };
+  }
+
+  static toNewEntity(dto: NoteDTO): NewNote {
+    try {
+      const parsedDTO = createNewNote.parse({
+        pageType: dto.page_type,
+        pageTitle: dto.page_title,
+        pageIcon: dto.page_icon,
+        pageColor: dto.page_color,
+        archived: dto.archived,
+        pinned: dto.pinned,
+        tag: dto.tag ? TagMapper.toNewEntity(dto.tag) : null,
+        noteContent: dto.note_content,
+      });
+      return parsedDTO;
+    } catch (error) {
+      console.error("Error mapping NoteDTO to New Entity:", error);
+      throw new Error("Failed to map NoteDTO to New Entity");
+    }
+  }
+
+  static toEntity(model: NoteModel): Note {
+    try {
+      return noteSchema.parse({
+        pageID: pageID.parse(model.pageID),
+        pageType: model.page_type,
+        pageTitle: model.page_title,
+        pageIcon: model.page_icon,
+        pageColor: model.page_color,
+        archived: model.archived === 1,
+        pinned: model.pinned === 1,
+        tag: model.tagID
+          ? TagMapper.toEntity({
+              tagID: model.tagID,
+              tag_label: model.tag_label!,
+            })
+          : null,
+        createdAt: new Date(model.date_created),
+        updatedAt: new Date(model.date_modified),
+        noteID: model.noteID,
+        noteContent: model.note_content,
+      });
+    } catch (error) {
+      console.error("Error mapping NoteModel to Entity:", error);
+      throw new Error("Failed to map NoteModel to Entity");
+    }
+  }
+}
