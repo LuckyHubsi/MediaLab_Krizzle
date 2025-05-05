20ca30f67c829f26afbf3b12ad35bb2c576766c9
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-05 22:39:12 +0200

    feat(Attribute): add domain entity.
    Add domain entity for Tags with zod validation.
    
    Refs: SCRUM-133

---
+++ backend/domain/common/Attribute.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+import * as common from "@/backend/domain/common/types";
+import { AttributeType } from "@/backend/util/enum/AttributeType";
+
+export const attributeID = common.positiveInt.brand<"AttributeId">();
+export type AttributeID = z.infer<typeof attributeID>;
+
+export const attributeSchema = z
+  .object({
+    attributeID: attributeID,
+    attributeLabel: common.string30,
+    type: z.nativeEnum(AttributeType),
+    preview: common.boolean,
+    options: z.array(common.string30).optional().nullable(),
+    symbol: common.optionalString.nullable(),
+  })
+  .refine(
+    (data) => {
+      if (data.type === AttributeType.Multiselect) {
+        return (
+          data.options !== null &&
+          data.options !== undefined &&
+          data.options.length >= 1 &&
+          data.options.length <= 20
+        );
+      }
+      return true;
+    },
+    {
+      message: "Multi-select attributes must have between 1 and 20 options.",
+      path: ["options"],
+    },
+  );
+
+export type Attribute = z.infer<typeof attributeSchema>;
+
+export const createNewAttributeSchema = z
+  .object({
+    attributeLabel: common.string30,
+    type: z.nativeEnum(AttributeType),
+    preview: common.boolean.default(false),
+    options: z.array(common.string30).optional().nullable(),
+    symbol: common.optionalString.nullable(),
+  })
+  .refine(
+    (data) => {
+      if (data.type === AttributeType.Multiselect) {
+        return (
+          data.options !== null &&
+          data.options !== undefined &&
+          data.options.length >= 1 &&
+          data.options.length <= 20
+        );
+      }
+      return true;
+    },
+    {
+      message: "Multi-select attributes must have between 1 and 20 options.",
+      path: ["options"],
+    },
+  );
+
+export type NewAttribute = z.infer<typeof createNewAttributeSchema>;
---
+++ backend/util/enum/AttributeType.ts
@@ -0,0 +1,14 @@
+/**
+ * Enum representing the different possible types of attributes within a collection template in the app.
+ *
+ * - `Text`: Represents a text attribute.
+ * - `Rating`: Represents a number attribute from 1-5.
+ * - `Date`: Represents a date attribute.
+ * - `Multiselect`: Represents a list of 'labels' grouped in the multi-select attribute.
+ */
+export enum AttributeType {
+  Text = "text",
+  Rating = "rating",
+  Date = "date",
+  Multiselect = "multi-select",
+}
