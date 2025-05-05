a9a31af21fe8fdd1c6d09a8ea649b7c2e28b2a64
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-05 22:49:15 +0200

    feat(AttributeMapper): add mapper class
    Add new Mapper class that maps between DTO/model and the domain entity.
    
    Refs: SCRUM-133

---
+++ backend/util/mapper/AttributeMapper.ts
@@ -0,0 +1,78 @@
+import {
+  Attribute,
+  attributeID,
+  attributeSchema,
+  createNewAttributeSchema,
+  NewAttribute,
+} from "@/backend/domain/common/Attribute";
+import { AttributeModel } from "@/backend/repository/model/AttributeModel";
+import { AttributeDTORestructure } from "@/dto/AttributeDTO";
+import { AttributeType } from "../enum/AttributeType";
+
+export class AttributeMapper {
+  static toDTO(entity: Attribute): AttributeDTORestructure {
+    return {
+      attributeID: entity.attributeID,
+      attributeLabel: entity.attributeLabel,
+      type: entity.type,
+      preview: entity.preview,
+      options: entity.options ?? undefined,
+      symbol: entity.symbol ?? undefined,
+    };
+  }
+
+  static toModel(entity: Attribute): AttributeModel {
+    return {
+      attributeID: entity.attributeID,
+      attribute_label: entity.attributeLabel,
+      type: entity.type,
+      preview: entity.preview ? 1 : 0,
+      options: entity.options ? JSON.stringify(entity.options) : null,
+      symbol: entity.symbol ?? null,
+    };
+  }
+
+  static toInsertModel(
+    entity: NewAttribute,
+  ): Omit<AttributeModel, "attributeID"> {
+    return {
+      attribute_label: entity.attributeLabel,
+      type: entity.type,
+      preview: entity.preview ? 1 : 0,
+      options: entity.options ? JSON.stringify(entity.options) : null,
+      symbol: entity.symbol ?? null,
+    };
+  }
+
+  static toNewEntity(dto: AttributeDTORestructure): NewAttribute {
+    try {
+      const parsedDTO = createNewAttributeSchema.parse({
+        attributeLabel: dto.attributeLabel,
+        type: dto.type,
+        preview: dto.preview,
+        options: dto.options,
+        symbol: dto.symbol,
+      });
+      return parsedDTO;
+    } catch (error) {
+      console.error("Error mapping AttributeDTO to New Entity.");
+      throw new Error("Failed to map AttributeDTO to New Entity");
+    }
+  }
+
+  static toEntity(model: AttributeModel): Attribute {
+    try {
+      return attributeSchema.parse({
+        attributeID: attributeID.parse(model.attributeID),
+        attributeLabel: model.attribute_label,
+        type: model.type as AttributeType,
+        preview: model.preview === 1,
+        options: model.options ? JSON.parse(model.options) : null,
+        symbol: model.symbol ?? null,
+      });
+    } catch (error) {
+      console.error("Error mapping AttributeModel to Entity");
+      throw new Error("Failed to map AttributeModel to Entity");
+    }
+  }
+}
