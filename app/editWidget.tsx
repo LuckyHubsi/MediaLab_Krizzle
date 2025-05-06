dbd6954253afcedc6efbb35cb497daa161c54627
LuckyHubsi <cc221040@fhstp.ac.at>
2025-05-06 17:36:19 +0200

    feat(editWidget): create editWidget page
    Use structure from createNote and add update functionality from backend into the frontend.
    
    Refs: #SCRUM-179

---
+++ app/editWidget.tsx
@@ -0,0 +1,297 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { router, useLocalSearchParams, useNavigation } from "expo-router";
+import { View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedView } from "@/components/ui/ThemedView/ThemedView";
+import Widget from "@/components/ui/Widget/Widget";
+import { Card } from "@/components/ui/Card/Card";
+import { Header } from "@/components/ui/Header/Header";
+import { Button } from "@/components/ui/Button/Button";
+import { TitleCard } from "@/components/ui/TitleCard/TitleCard";
+import { TagPicker } from "@/components/ui/TagPicker/TagPicker";
+import { ChooseCard } from "@/components/ui/ChooseCard/ChooseCard";
+import { ChoosePopup } from "@/components/ui/ChoosePopup/ChoosePopup";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "react-native";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import {
+  colorLabelMap,
+  colorKeyMap,
+  iconLabelMap,
+} from "@/constants/LabelMaps";
+import { Icons } from "@/constants/Icons";
+import { NoteDTO } from "@/dto/NoteDTO";
+import { PageType } from "@/utils/enums/PageType";
+import { insertNote } from "@/services/NoteService";
+import { TagDTO } from "@/dto/TagDTO";
+import { ThemedText } from "@/components/ThemedText";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
+import { DividerWithLabel } from "@/components/ui/DividerWithLabel/DividerWithLabel";
+import { getAllTags } from "@/services/TagService";
+import { useFocusEffect } from "@react-navigation/native";
+import { GeneralPageDTO } from "@/dto/GeneralPageDTO";
+import {
+  getGeneralPageByID,
+  updateGeneralPageData,
+} from "@/services/GeneralPageService";
+import { set } from "date-fns";
+
+export default function EditWidgetScreen() {
+  const navigation = useNavigation();
+  const colorScheme = useColorScheme();
+  const { widgetID } = useLocalSearchParams<{ widgetID: string }>();
+  const [pageData, setPageData] = useState<GeneralPageDTO | null>();
+  const [title, setTitle] = useState("");
+  const [selectedTag, setSelectedTag] = useState<TagDTO | null>(null);
+  const [selectedColor, setSelectedColor] = useState<string>("");
+  const [selectedIcon, setSelectedIcon] = useState<
+    keyof typeof MaterialIcons.glyphMap | null
+  >(null);
+  const [titleError, setTitleError] = useState<string | null>(null);
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [popupType, setPopupType] = useState<"color" | "icon">("color");
+  const [tags, setTags] = useState<TagDTO[]>([]);
+
+  const selectedColorLabel = colorLabelMap[selectedColor] || "Choose Color";
+  const selectedIconLabel = selectedIcon
+    ? iconLabelMap[selectedIcon]
+    : "Choose Icon";
+
+  const colorOptions = Object.entries(Colors.widget).map(([key, value]) => {
+    const label = colorLabelMap[Array.isArray(value) ? value[0] : value] ?? key;
+
+    return {
+      id: key,
+      color: value,
+      value: key,
+      label,
+    };
+  });
+
+  const getWidgetColorKey = (
+    value: string,
+  ): keyof typeof Colors.widget | undefined => {
+    return Object.keys(Colors.widget).find(
+      (key) =>
+        (typeof Colors.widget[key as keyof typeof Colors.widget] === "string" &&
+          Colors.widget[key as keyof typeof Colors.widget] === value) ||
+        (Array.isArray(Colors.widget[key as keyof typeof Colors.widget]) &&
+          Array.isArray(Colors.widget[key as keyof typeof Colors.widget]) &&
+          Colors.widget[key as keyof typeof Colors.widget].includes(value)),
+    ) as keyof typeof Colors.widget | undefined;
+  };
+
+  const updateWidget = async () => {
+    if (title.trim().length === 0) {
+      setTitleError("Title is required.");
+      return;
+    }
+
+    setTitleError(null);
+
+    let tagDTO: TagDTO | null = null;
+
+    if (selectedTag !== null) {
+      const matchingTag = tags.find(
+        (tag) => tag.tag_label === selectedTag?.tag_label,
+      );
+      if (matchingTag) {
+        tagDTO = {
+          tagID: matchingTag.tagID,
+          tag_label: matchingTag.tag_label,
+        };
+      }
+    }
+
+    await updateGeneralPageData(
+      Number(widgetID),
+      title,
+      selectedIcon || "",
+      selectedColor,
+      tagDTO?.tagID || null,
+    );
+
+    router.back();
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchTags = async () => {
+        try {
+          const tagData = await getAllTags();
+          if (tagData) setTags(tagData);
+        } catch (error) {
+          console.error("Failed to load tags:", error);
+        }
+      };
+
+      const fetchGeneralPage = async () => {
+        try {
+          const generalPageData = await getGeneralPageByID(Number(widgetID));
+          console.log("General page data:", generalPageData);
+          if (generalPageData) {
+            setPageData(generalPageData);
+            setTitle(generalPageData.page_title || "");
+            setSelectedColor(generalPageData.page_color || "");
+            setSelectedIcon(
+              (generalPageData.page_icon as
+                | keyof typeof MaterialIcons.glyphMap
+                | null) || null,
+            );
+            setSelectedTag(generalPageData.tag || null);
+          }
+        } catch (error) {
+          console.error("Failed to load page data:", error);
+        }
+      };
+
+      fetchTags();
+      fetchGeneralPage();
+    }, []),
+  );
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemedView style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 10 }}>
+          <View style={{ flex: 1, alignItems: "center", gap: 20 }}>
+            <Card>
+              <View style={{ alignItems: "center" }}>
+                <Header
+                  title="Edit Widget"
+                  onIconPress={() => alert("Popup!")}
+                />
+                <Widget
+                  title={title || "Title"}
+                  label={selectedTag?.tag_label ?? "No tag"}
+                  pageType={PageType.Note}
+                  iconLeft={
+                    <MaterialIcons
+                      name={selectedIcon || "help"}
+                      size={20}
+                      color="black"
+                    />
+                  }
+                  iconRight={
+                    <MaterialIcons name="description" size={20} color="black" />
+                  }
+                  color={
+                    (getWidgetColorKey(
+                      selectedColor,
+                    ) as keyof typeof Colors.widget) || "#4599E8"
+                  }
+                />
+              </View>
+            </Card>
+
+            <View style={{ width: "100%", gap: 20 }}>
+              <Card>
+                <TitleCard
+                  placeholder="Add a title to your Note"
+                  value={title}
+                  onChangeText={(text) => {
+                    setTitle(text);
+                    if (text.trim().length > 0) setTitleError(null); // clear error while typing
+                  }}
+                />
+                {titleError && (
+                  <ThemedText
+                    style={{
+                      marginTop: 5,
+                    }}
+                    fontSize="s"
+                    colorVariant="red"
+                  >
+                    {titleError}
+                  </ThemedText>
+                )}
+              </Card>
+              <DividerWithLabel label="optional" iconName="arrow-back" />
+              <Card>
+                <TagPicker
+                  tags={tags}
+                  selectedTag={selectedTag}
+                  onSelectTag={(tag) => {
+                    setSelectedTag((prevTag) => (prevTag === tag ? null : tag));
+                  }}
+                  onViewAllPress={() => router.push("/tagManagement")}
+                />
+              </Card>
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: "100%",
+                  justifyContent: "space-between",
+                  gap: 15,
+                }}
+              >
+                <View style={{ flex: 1 }}>
+                  <ChooseCard
+                    label={selectedColorLabel}
+                    selectedColor={selectedColor}
+                    onPress={() => {
+                      setPopupType("color");
+                      setPopupVisible(true);
+                    }}
+                  />
+                </View>
+
+                <View style={{ flex: 1 }}>
+                  <ChooseCard
+                    label={selectedIconLabel}
+                    selectedColor={
+                      useColorScheme() === "dark"
+                        ? Colors.dark.cardBackground
+                        : Colors.light.cardBackground
+                    }
+                    selectedIcon={selectedIcon ?? undefined}
+                    onPress={() => {
+                      setPopupType("icon");
+                      setPopupVisible(true);
+                    }}
+                  />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              width: "100%",
+            }}
+          >
+            <Button onPress={updateWidget}>Save</Button>
+          </View>
+        </ScrollView>
+        <ChoosePopup
+          visible={popupVisible}
+          type={popupType}
+          items={
+            popupType === "color"
+              ? colorOptions.map((option, index) => ({
+                  id: `${option.color}-${index}`,
+                  value: option.color,
+                  label: option.label,
+                }))
+              : Icons.map((iconName, index) => ({
+                  id: `${iconName}-${index}`,
+                  value: iconName,
+                }))
+          }
+          selectedItem={popupType === "color" ? selectedColor : selectedIcon}
+          onSelect={(itemValue) => {
+            if (popupType === "color") {
+              setSelectedColor(itemValue);
+            } else {
+              setSelectedIcon(itemValue as keyof typeof MaterialIcons.glyphMap);
+            }
+          }}
+          onClose={() => setPopupVisible(false)}
+          onDone={() => setPopupVisible(false)}
+        />
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
