5e502d5fd72c9233bb26179651993e6bde3d1aa5
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-06 09:08:17 +0200

    refactor(getAllTags): add catch block
    Add catch block for consistency with other service methods.
    
    Refs: SCRUM-133

--- backend/repository/implementation/TagRepository.implementation.ts
+++ backend/repository/implementation/TagRepository.implementation.ts
@@ -4,14 +4,19 @@ import { TagRepository } from "../interfaces/TagRepository.interface";
 import { selectAllTagsQuery } from "../query/TagQuery";
 import { TagMapper } from "@/backend/util/mapper/TagMapper";
 import { TagModel } from "../model/TagModel";
+import { RepositoryError } from "@/backend/util/error/RepositoryError";
 
 export class TagRepositoryImpl
   extends BaseRepositoryImpl
   implements TagRepository
 {
   async getAllTags(): Promise<Tag[]> {
-    const result = await this.fetchAll<TagModel>(selectAllTagsQuery);
-    return result.map(TagMapper.toEntity);
+    try {
+      const result = await this.fetchAll<TagModel>(selectAllTagsQuery);
+      return result.map(TagMapper.toEntity);
+    } catch (error) {
+      throw new RepositoryError("Failed to fetch all tags.");
+    }
   }
 }
 
