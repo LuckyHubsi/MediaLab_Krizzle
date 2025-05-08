d4bafa63d2a5479c2a70eb588fb723548516b0d7
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-05-08 11:34:29 +0200

    refactor(QuickActionModal): fix hook error when pinning the 4th Widget
     fix hook error when pinning the 4th Widget

--- components/Modals/QuickActionModal/QuickActionModal.tsx
+++ components/Modals/QuickActionModal/QuickActionModal.tsx
@@ -44,48 +44,51 @@ export default function QuickActionModal({
         <ModalBackground>
           <TouchableOpacity activeOpacity={1}>
             <ModalBox colorScheme={colorScheme}>
-              {items.map((item, index) => (
-                <PopupItem
-                  key={index}
-                  onPress={() => {
-                    if (!item.disabled) {
-                      item.onPress?.();
-                      onClose();
-                    }
-                  }}
-                  colorScheme={colorScheme}
-                  isLast={index === items.length - 1}
-                  disabled={item.disabled}
-                >
-                  <ThemedText
-                    fontSize="regular"
-                    fontWeight="regular"
-                    colorVariant={
-                      item.disabled
-                        ? "disabled"
-                        : item.danger
-                          ? "red"
-                          : "default"
-                    }
-                  >
-                    {item.label}
-                  </ThemedText>
+              {items.map((item, index) => {
+                // Pre-calculate the correct colorVariant and icon color
+                const textColorVariant = item.disabled
+                  ? "disabled"
+                  : item.danger
+                    ? "red"
+                    : "default";
 
-                  <IconContainer>
-                    <MaterialIcons
-                      name={item.icon}
-                      size={20}
-                      color={
-                        item.disabled
-                          ? Colors[colorScheme].disabled
-                          : item.danger
-                            ? Colors.negative
-                            : Colors[colorScheme].text
+                const iconColor = item.disabled
+                  ? Colors[colorScheme].disabled
+                  : item.danger
+                    ? Colors.negative
+                    : Colors[colorScheme].text;
+
+                return (
+                  <PopupItem
+                    key={index}
+                    onPress={() => {
+                      if (!item.disabled) {
+                        item.onPress?.();
+                        onClose();
                       }
-                    />
-                  </IconContainer>
-                </PopupItem>
-              ))}
+                    }}
+                    colorScheme={colorScheme}
+                    isLast={index === items.length - 1}
+                    disabled={item.disabled}
+                  >
+                    <ThemedText
+                      fontSize="regular"
+                      fontWeight="regular"
+                      colorVariant={textColorVariant}
+                    >
+                      {item.label}
+                    </ThemedText>
+
+                    <IconContainer>
+                      <MaterialIcons
+                        name={item.icon}
+                        size={20}
+                        color={iconColor}
+                      />
+                    </IconContainer>
+                  </PopupItem>
+                );
+              })}
             </ModalBox>
           </TouchableOpacity>
         </ModalBackground>
