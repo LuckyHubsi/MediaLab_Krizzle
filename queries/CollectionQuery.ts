026705908916fc657f0b45411ff9f5f194b1f7ce
michi-992 <152781506+michi-992@users.noreply.github.com>
2025-05-22 10:52:21 +0200

    fix(collectionSelectByPageIdQuery): order categories
    Adapt the collection categories to be retrieved already sorted by their ID ascending.

--- queries/CollectionQuery.ts
+++ queries/CollectionQuery.ts
@@ -1,23 +1,27 @@
 const collectionSelectByPageIdQuery: string = `
     SELECT
-        p.*,
-        c.collectionID,
-        (
-            SELECT COUNT(*)
-            FROM general_page_data p
-            WHERE p.pinned = 1
-        ) AS pin_count,
-        c.item_templateID AS templateID,
-        json_group_array(
-            DISTINCT json_object(
-                'collection_categoryID', cc.collection_categoryID,
-                'category_name', cc.category_name,
-                'collectionID', cc.collectionID
-            )
-        ) AS categories
+    p.*,
+    c.collectionID,
+    (
+        SELECT COUNT(*)
+        FROM general_page_data p2
+        WHERE p2.pinned = 1
+    ) AS pin_count,
+    c.item_templateID AS templateID,
+    (
+        SELECT json_group_array(json_object(
+            'collection_categoryID', cc.collection_categoryID,
+            'category_name', cc.category_name,
+            'collectionID', cc.collectionID
+        ))
+        FROM (
+            SELECT * FROM collection_category
+            WHERE collectionID = c.collectionID
+            ORDER BY collection_categoryID ASC
+        ) cc
+    ) AS categories
     FROM general_page_data p
     JOIN collection c ON p.pageID = c.pageID
-    LEFT JOIN collection_category cc ON c.collectionID = cc.collectionID
     WHERE p.pageID = ?
     GROUP BY p.pageID, c.collectionID;
 `;
