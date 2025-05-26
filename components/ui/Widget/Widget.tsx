7fe1c5a266d4f00f7e2f6131ef8ae581a1b5ae56
LuckyHubsi <cc221040@fhstp.ac.at>
2025-05-26 20:45:29 +0200

    feat(Widget): add accessibility props
    Add properties to the widget component to enhance screen reader accessibility.
    
    Refs: #SCRUM-223

--- components/ui/Widget/Widget.tsx
+++ components/ui/Widget/Widget.tsx
@@ -64,11 +64,19 @@ const Widget: React.FC<Props> = ({
     }
   };
 
+  const iconName =
+    React.isValidElement(icon) && icon.props?.name
+      ? icon.props.name
+      : "unknown icon";
+
   return (
     <TouchableOpacity
       activeOpacity={0.85}
       onPress={onPress}
       onLongPress={handleLongPress}
+      accessibilityRole="button"
+      accessibilityLabel={`Open ${pageType} titled ${title} with tag ${label} and icon ${iconName} and color ${color}`}
+      accessibilityHint="Double tap to open. Long press for more options."
     >
       <CardWrapper {...cardProps}>
         {isPreview && (
