7916f2c48a3d40b73cfc6d6f7f9ea3a242e79bc7
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-06-03 18:52:51 +0200

    refactor(notePage): change import of colorScheme to ActiveColorScheme
    change import of colorScheme to ActiveColorScheme

--- app/notePage.tsx
+++ app/notePage.tsx
@@ -10,7 +10,6 @@ import { NoteDTO } from "@/shared/dto/NoteDTO";
 import DeleteModal from "@/components/Modals/DeleteModal/DeleteModal";
 import { useState } from "react";
 import { useDebouncedCallback } from "@/hooks/useDebouncedCallback";
-import { useColorScheme } from "react-native";
 import QuickActionModal, {
   QuickActionItem,
 } from "@/components/Modals/QuickActionModal/QuickActionModal";
@@ -22,6 +21,7 @@ import SelectFolderModal from "@/components/ui/SelectFolderModal/SelectFolderMod
 import { EnrichedError } from "@/shared/error/ServiceError";
 import { ErrorPopup } from "@/components/Modals/ErrorModal/ErrorModal";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useActiveColorScheme } from "@/context/ThemeContext";
 
 export default function NotesScreen() {
   const { pageId, title, routing } = useLocalSearchParams<{
@@ -34,7 +34,7 @@ export default function NotesScreen() {
   const router = useRouter();
   const [noteContent, setNoteContent] = useState<string>("");
   const latestNoteContentRef = useRef<string>("");
-  const colorScheme = useColorScheme();
+  const colorScheme = useActiveColorScheme();
   const [showModal, setShowModal] = useState(false);
   const [noteData, setNoteData] = useState<NoteDTO | null>();
   const [shouldReload, setShouldReload] = useState<boolean>();
