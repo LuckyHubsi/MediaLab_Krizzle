b45ec06506a614ccf0b78698c8dbf9223a7f7bc8
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-06-05 16:19:27 +0200

    refactor(FAB): documentation and clean up
    Documentation and clean up

--- components/ui/NavBar/FloatingAddButton/FloatingAddButton.tsx
+++ components/ui/NavBar/FloatingAddButton/FloatingAddButton.tsx
@@ -4,9 +4,16 @@ import { GestureResponderEvent } from "react-native";
 import { StyledButtonContainer } from "./FloatingAddButton.styles";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useActiveColorScheme } from "@/context/ThemeContext";
+import { Colors } from "@/constants/Colors";
+
+/**
+ * Component for a floating action button that triggers an action when pressed.
+ *
+ * @param onPress (required) - Callback function to handle press events.
+ */
 
 type FloatingAddButtonProps = {
-  onPress?: (event: GestureResponderEvent) => void;
+  onPress: (event: GestureResponderEvent) => void;
 };
 
 export const FloatingAddButton: React.FC<FloatingAddButtonProps> = ({
@@ -16,7 +23,7 @@ export const FloatingAddButton: React.FC<FloatingAddButtonProps> = ({
 
   return (
     <StyledButtonContainer onPress={onPress} colorScheme={color}>
-      <MaterialIcons name="add" size={40} color={"white"} />
+      <MaterialIcons name="add" size={40} color={Colors.white} />
     </StyledButtonContainer>
   );
 };
