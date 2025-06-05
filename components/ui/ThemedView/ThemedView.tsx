65a36b60bace9bd93dfc02c3f313759be1e3c762
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-06-05 17:37:03 +0200

    refactor(ThemedView): documentation and clean up
    Documentation and clean up

--- components/ui/ThemedView/ThemedView.tsx
+++ components/ui/ThemedView/ThemedView.tsx
@@ -2,6 +2,14 @@ import { ViewProps } from "react-native";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { StyledView } from "./ThemedView.styles";
 
+/**
+ * Component for rendering a themed view padding and background color based on the active color scheme.
+ *
+ * @param lightColor - The color for light mode background.
+ * @param darkColor - The color for dark mode background.
+ * @param topPadding - The padding at the top of the content (default: 20).
+ */
+
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
