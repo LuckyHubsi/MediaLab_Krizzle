7abf205ee20148cd97854f3dbf103aa08c7f9aab
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2025-06-13 22:14:59 +0200

    refactor(Widget): make Preview of the Widget smaller
    Make Preview of the Widget smaller, so there is more space an small screens

--- components/ui/Widget/Widget.style.ts
+++ components/ui/Widget/Widget.style.ts
@@ -4,14 +4,18 @@ type CardProps = { cardWidth: number };
 
 export const CardSolid = styled.View<CardProps>`
   width: ${({ cardWidth }: CardProps) => `${cardWidth}px`};
+  padding: ${({ padding }: { padding: number }) => `${padding}px`};
   aspect-ratio: 1;
   border-radius: 33px;
-  padding: 20px;
   justify-content: flex-end;
 `;
 
-export const CardGradient = styled(LinearGradient)<{ cardWidth: number }>`
+export const CardGradient = styled(LinearGradient)<{
+  cardWidth: number;
+  padding: number;
+}>`
   width: ${({ cardWidth }: CardProps) => `${cardWidth}px`};
+  padding: ${({ padding }: { padding: number }) => `${padding}px`};
   aspect-ratio: 1;
   border-radius: 33px;
   padding: 20px;
