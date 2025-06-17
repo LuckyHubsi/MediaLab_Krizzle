c0a5820e83d8c6934919cbc3bd302c89e1733f46
maxima <cc221022@fhstp.ac.at>
2025-06-17 22:42:23 +0200

    feat(appearance): add documenation

--- app/appearance.tsx
+++ app/appearance.tsx
@@ -1,4 +1,3 @@
-import { router } from "expo-router";
 import { ThemedView } from "@/components/ui/ThemedView/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,11 +7,16 @@ import { ThemeSelector } from "@/components/ui/ThemeSelector/ThemeSelector";
 import { useUserTheme } from "@/context/ThemeContext";
 import { useColorScheme, ActivityIndicator } from "react-native";
 
+/**
+ * AppearanceScreen component allows users to select their preferred theme (light/dark/system).
+ */
+
 export default function AppearanceScreen() {
   const { userTheme, saveUserTheme, isLoading } = useUserTheme();
   const systemColorScheme = useColorScheme() ?? "light";
   const selectedTheme = userTheme === "system" ? systemColorScheme : userTheme;
 
+  // If the user theme is loading, show a loading indicator
   if (isLoading) {
     return (
       <SafeAreaView>
